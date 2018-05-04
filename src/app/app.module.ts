ce0f47d60d7e414430f760e2f25514605a948f30
Wendy Tatiana Alzate Cardona <walzate@todo1.com>
2018-05-04 15:01:05 -0500

    Comunication between components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule
